841dbc9b4bf9e95337b237d06c168715e4dc526b
Guy <guyskv@gmail.com>
2020-05-25 20:59:18 +0300

    add stop listener

--- Class Location/ViewController.swift
+++ Class Location/ViewController.swift
@@ -1,20 +1,42 @@
-//
-//  ViewController.swift
-//  Class Location
-//
-//  Created by Guy Macin on 25/05/2020.
-//  Copyright © 2020 Guy Macin. All rights reserved.
-//
-
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+
+        locationManager.requestWhenInUseAuthorization()
+        
+   
     }
+    
+    @IBAction func getLocationPressed(_ sender: UIButton) {
+             locationManager.requestLocation()
+    }
+    
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations")
 
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("got Location: \(lat) \(lon)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error=\(error)")
+    }
 }
 
