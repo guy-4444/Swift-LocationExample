5bbd90f0d97f602e368719108e717e761284f201
Guy <guyskv@gmail.com>
2020-05-25 19:47:31 +0300

    Initial Commit

---
+++ Class Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class Location
+//
+//  Created by Guy Macin on 25/05/2020.
+//  Copyright © 2020 Guy Macin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
